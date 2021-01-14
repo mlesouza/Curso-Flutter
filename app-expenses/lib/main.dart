bf2a3f1f6e059717916e2acc9227740bf1a4be7b
marcelo souza <mlesouza95@gmail.com>
2021-01-14 16:59:34 -0300

    adicionado modelo transaction

--- app-expenses/lib/main.dart
+++ app-expenses/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -16,12 +17,41 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 30.76,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Despesas Pessoais')),
-      body: Center(
-        child: Text('Versão Inicial'),
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      backgroundColor: Colors.grey.shade300,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            child: Card(
+              elevation: 5,
+              child: Text('Lista de Transações'),
+            ),
+          )
+        ],
       ),
     );
   }
